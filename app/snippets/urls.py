078734df0b00c7892ccd7cdcde97110595befe5e
jihye <zehye.01@gmail.com>
2020-02-11 16:31:12 +0900

    snippet_list, snippet_detail view 생성

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import snippet_list
+from .views import snippet_list, snippet_detail
 
 app_name = 'snippets'
 urlpatterns = [
     path('snippets/', snippet_list),
+    path('snippets/<int:pk>/', snippet_detail),
 ]
--- app/snippets/views.py
+++ app/snippets/views.py
@@ -1,6 +1,7 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import JSONParser
 
 from .models import Snippets
@@ -22,3 +23,23 @@ def snippet_list(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.data, status=400)
 
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    snippet = get_object_or_404(Snippets, pk=pk)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PATCH':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
